1303d84431b701dcf765d928bda913eb91dedc2f
Sjgandhi1998 <shivam.jgandhi@gmail.com>
2018-12-20 09:25:22 -0500

    Continued with the utils file

--- triangleSim.py
+++ triangleSim.py
@@ -1,3 +1,24 @@
 import numpy as np
 import math
+from utils import *
+import argparse
+import os
 
+
+def arg_parse():
+    """
+    :return: Arguments for triangle simulation
+    """
+    parser = argparse.ArgumentParser(description='Isosceles Triangle Simulation')
+    parser.add_argument("--angle", dest="angle", help="Primary angle of the isoscles triangle",
+                        default=60, type=int)
+    parser.add_argument("--number", dest="N", help="Number of triangles to simulate",
+                        default=100, type=int)
+    return parser.parse_args()
+
+
+def main():
+    args = arg_parse()
+    angle = args['angle']
+    N = args['N']
+    triangleCoodinates = generateTriangles(angle, N)
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import matplotlib.lines as mlines
 import numpy as np
 import math
 
+
 def newline(p1, p2):
     """
     This function creates a new line through p1 and p2 spanning the whole plot
@@ -14,13 +15,36 @@ def newline(p1, p2):
     ax = plt.gca()
     xmin, xmax = ax.get_xbound()
 
-    if (p2[0] == p1[0]):
+    if p2[0] == p1[0]:
         xmin = xmax = p1[0]
         ymin, ymax = ax.get_ybound()
     else:
-        ymax = p1[1] + (p2[1] - p1[1])/(p2[0] - p1[0])*(xmax-p1[0])
-        ymin = p1[1] + (p2[1] - p1[1])/(p2[0] - p1[0])*(xmin-p1[0])
+        ymax = p1[1] + (p2[1] - p1[1]) / (p2[0] - p1[0]) * (xmax - p1[0])
+        ymin = p1[1] + (p2[1] - p1[1]) / (p2[0] - p1[0]) * (xmin - p1[0])
 
     l = mlines.Line2D([xmin, xmax], [ymin, ymax])
     ax.add_line(l)
     return l
+
+
+def generateTriangles(angle, N):
+    """
+
+    :param angle: the main angle of the isosceles triangles being simulated
+    :param N: The number of triangles to be generated
+    :return: triangleCoordinates: the coordinates of the N acute triangle's points
+    """
+
+    # generate the first triangle
+    point1 = [0, 0]
+    point2 = 
+
+    return triangleCoordinates
+
+
+def drawTriangeles(coordinates):
+    """
+
+    :param coordinates: The triples containing the coordinates of the triangles's points
+    :return:
+    """
