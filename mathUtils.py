35d0e0ada1455643e38863face30e1fb044e30c7
Sjgandhi1998 <shivam.jgandhi@gmail.com>
2018-12-20 22:49:09 -0500

    More code

--- mathUtils.py
+++ mathUtils.py
@@ -18,5 +18,17 @@ def intersection(boundary, triangle):
     :param triangle: 3x2 array with the 3 points of the triangle
     :return isIntersection: tells whether or not the new triangle intersects the boundary
     """
+    isIntersection = False
+    [a, b] = boundary.shape
+    for i in range(0, b):
+        point1 = boundary[0:2, b]
+        point2 = boundary[0:2, b+1]
+        mBoundary = (point2[1]-point1[1])/(point2[0] - point1[0])
+        bBoundary = point1[1] - mBoundary*point1[0]
+        # Check if line 1 intersects
 
+
+        # Check if line 2 intersects
+
+        # Check if line 3 intersects
     return isIntersection
