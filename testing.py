971a3b5d919817566b80ea107dcc1e6d71fd111b
Sjgandhi1998 <shivam.jgandhi@gmail.com>
2019-01-07 06:23:38 -0500

    Issue of dividing by zero

--- testing.py
+++ testing.py
@@ -65,6 +65,7 @@ class mathUtilsTest(unittest.TestCase):
         newTriangle.setCoordinates([[1.0, 1.0],
                                     [2.0, 1.0],
                                     [1.5, 1.5]])
+        self.assertEqual(intersection(newBoundary, newTriangle), False)
 
 
 class triangleClassTest(unittest.TestCase):
