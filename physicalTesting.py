7ddf6e84df6abe137b3c1e579b57f1290775ccda
Sjgandhi1998 <shivam.jgandhi@gmail.com>
2019-02-19 17:54:39 -0500

    started the physicalTesting script

---
+++ physicalTesting.py
@@ -0,0 +1,22 @@
+import unittest
+import math
+import numpy as np
+
+from triangleClass import *
+from utils import *
+from mathUtils import *
+
+""" These are our 0th order functions, base functions unused by others"""
+
+# The following functions are tests for packing functions
+
+# TODO: complete this test
+class randomEdgeTest(unittest.TestCase):
+
+# TODO: complete this test
+class newRadiusTest(unittest.TestCase):
+
+# TODO: complete this test
+class updatePackingTest(unittest.TestCase):
+
+""" These are our first order functions"""
