2509340ac4f9ee8a7fdd1c265c4870b1ded4dfec
Sjgandhi1998 <shivam.jgandhi@gmail.com>
2019-02-19 22:24:42 -0500

    continued the test cases

--- physicalTesting.py
+++ physicalTesting.py
@@ -13,13 +13,28 @@ from mathUtils import *
 class packingTest(unittest.TestCase):
 
     # TODO: complete this test
-    def randomEdgeTest(self):
+    def testAll(self):
 
-    # TODO: complete this test
-    def newRadiusTest(self):
+        # TODO: complete the creation of the test packings
+        # First we create the packings that we want to operate on
+        angle = toRadians(75)
+        point1 = [0, 0]
+        point2 = [0, 1]
+        point3 = [round3(1/(2*math.tan(angle / 2))), 1/2]
 
-    # TODO: complete this test
-    def updatePackingTest(self):
+        triangle_coordinates = np.asarray([point1, point2, point3])
+        first_triangle = triangle(triangle_coordinates, 1)
+        first_packing = randomPacking(first_triangle.coordinates, 1, [first_triangle])
+
+        # TODO: complete this test
+        # randomEdgeTest
+
+        # TODO: complete this test
+        # newRadiusTest
+
+        # TODO: complete this test
+        # updatePackingTest
+
 
 """ These are our first order functions"""
 
